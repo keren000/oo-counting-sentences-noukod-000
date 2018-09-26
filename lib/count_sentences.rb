5bef98707060ad898b50f4ab29d207c19fd35893
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-26 17:00:18 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    count = self.split(/[.?!]/) 
-    count.delete.if{|sentences| sentences.length ==0}
+    count = self.split(/[.?!]/)
+    count.delete.if{|sentences| sentences.length == 0}
   end
 end
