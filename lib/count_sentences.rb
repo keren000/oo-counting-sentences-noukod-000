02a1df934f8955f259a2744c6ea81fe72846b38f
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-26 16:54:56 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,11 +7,11 @@ class String
   end
 
   def question?
-    self.end_with?("?") ? true : false 
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-    "HI!".end_with?("!")
+    self.end_with?("!") ? true : false 
   end
 
   def count_sentences
